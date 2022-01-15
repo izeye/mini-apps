e7555ab659f9660d13e131b348be4b5082eb8aec
izeye <izeye@naver.com>
2022-01-15 21:36:57 +0900

    Polish

--- src/main/java/com/izeye/miniapps/MultiplicationPractice.java
+++ src/main/java/com/izeye/miniapps/MultiplicationPractice.java
@@ -18,8 +18,9 @@ public class MultiplicationPractice {
         List<String> questions = new ArrayList<>();
         List<Integer> answers = new ArrayList<>();
         for (int i = 0; i < NUMBER_OF_QUESTIONS; i++) {
-            int number1 = RandomUtils.generateNumber(100, 999);
-            int number2 = RandomUtils.generateNumber(10, 99);
+//            int number1 = getThreeDigitNumber();
+            int number1 = getTwoDigitNumber();
+            int number2 = getTwoDigitNumber();
             questions.add(number1 + " * " + number2 + " = ");
             answers.add(number1 * number2);
         }
@@ -34,6 +35,14 @@ public class MultiplicationPractice {
         for (int i = 0; i < NUMBER_OF_QUESTIONS; i++) {
             System.out.println((i + 1) + ": " + questions.get(i) + answers.get(i));
         }
+    }
+
+    private static int getTwoDigitNumber() {
+        return RandomUtils.generateNumber(10, 99);
+    }
+
+    private static int getThreeDigitNumber() {
+        return RandomUtils.generateNumber(100, 999);
     }
 
 }
